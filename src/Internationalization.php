af4580fd10f134ca0e04fe838267ad873833f1ac
TingSong-Syu <rek@rek.me>
2017-04-11 01:11:45 +0800

    i18n class

---
+++ src/Internationalization.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: rek
+ * Date: 2016/3/22
+ * Time: 下午4:37
+ */
+
+namespace x2ts\i18n;
+
+use x2ts\Component;
+
+class Internationalization extends Component {
+    protected static $_conf = [
+        'default' => 'En',
+    ];
+
+    protected static $messages = [];
+
+    public static function getInstance($language = null) {
+        if (!empty($language)) {
+            $class = "\\lang\\$language";
+            return new $class();
+        }
+        if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+            preg_match_all(
+                '#([\w\-, ]+); ?q=([01]\.\d)#',
+                $_SERVER['HTTP_ACCEPT_LANGUAGE'],
+                $m,
+                PREG_SET_ORDER
+            );
+            usort($m, function ($a, $b) {
+                $a = (float) $a[2] * 100;
+                $b = (float) $b[2] * 100;
+                return $b - $a;
+            });
+            $langs = [];
+            foreach ($m as $item) {
+                $group = explode(',', $item[1]);
+                foreach ($group as $lang) {
+                    $lang = trim($lang);
+                    if (!empty($lang))
+                        $langs[] = str_replace('-', '', ucfirst($lang));
+                }
+            }
+            foreach ($langs as $lang) {
+                $class = '\\lang\\' . $lang;
+                if (class_exists($class)) {
+                    return new $class();
+                }
+            }
+        }
+
+        $class = '\\lang\\' . static::$_conf['default'];
+        return new $class();
+    }
+}
---
+++ tests/I18nTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: rek
+ * Date: 2017/4/11
+ * Time: AM1:08
+ */
+
+namespace x2ts\i18n;
+
+use PHPUnit\Framework\TestCase;
+
+class I18nTest extends TestCase {
+
+}
